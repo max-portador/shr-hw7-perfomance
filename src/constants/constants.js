2db162a63d8991f250416f920488c8747a76455f
Fake-ignat <46213974+Fake-ignat@users.noreply.github.com>
2021-10-28 15:20:42 +0300

    Рефактор - вынес HEADER в отдельный компонент Добавил адаптиную верстку

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 const constants = {
     IMAGES_AMOUNT: 8,
-    GUID: "77d20161-ec5b-4cc1-b324-723096f7e4c7",
+    // GUID: "77d20161-ec5b-4cc1-b324-723096f7e4c7",
+    GUID: "23dc66db-ec6f-4314-89f8-f1d2256dfd39",
 };
 
 module.exports = constants;
