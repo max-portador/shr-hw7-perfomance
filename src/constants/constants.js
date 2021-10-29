29d9c48e97b5eed47da5dd63621655b4bdca9135
M.Shabanov <deus7@protonmail.com>
2021-10-29 13:13:27 +0300

    Change GUID

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,8 +6,8 @@ const constants = {
         cats: "loadCatsImages",
         randImages: "loadRandomImages",
     },
-    // GUID: "77d20161-ec5b-4cc1-b324-723096f7e4c7",
-    GUID: "23dc66db-ec6f-4314-89f8-f1d2256dfd39",
+    GUID: "77d20161-ec5b-4cc1-b324-723096f7e4c7",
+    // GUID: "23dc66db-ec6f-4314-89f8-f1d2256dfd39",
 };
 
 module.exports = constants;
