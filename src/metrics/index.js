709a1c39e3b6a850eba7d5d5dad894eab23b05ea
M.Shabanov <deus7@protonmail.com>
2021-10-29 15:22:35 +0300

    Исправил баг для Safari

--- src/metrics/index.js
+++ src/metrics/index.js
@@ -41,7 +41,7 @@ const getPlatform = () => {
 };
 
 const getConnectionType = () => {
-    if (navigator.connection["effectiveType"]) {
+    if ("effectiveType" in navigator.connection) {
     return navigator.connection["effectiveType"]
     // return string? for example "4g", don`t work in Safari
     }
